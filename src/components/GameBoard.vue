80a5c2a70fd10d0804a7532c5803b4b735185a80
Jonas Vaneeckhaute <jonastic.dev@gmail.com>
2025-04-13 14:34:00 +0200

    Add base html/css

---
+++ src/components/GameBoard.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="board">
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+  </div>
+</template>
+
+<style scoped>
+    .board {
+      display: grid;
+      grid-template-columns: repeat(3, 100px);
+      grid-template-rows: repeat(3, 100px);
+      gap: 5px;
+      padding: 5px;
+    }
+
+    .cell {
+      width: 100px;
+      height: 100px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: #fff;
+      border: 1px solid #ccc;
+      font-size: 32px;
+      cursor: pointer;
+    }
+
+    .cell:hover {
+      background-color: #eaeaea;
+    }
+</style>
